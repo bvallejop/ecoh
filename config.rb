39ed00dd386d0ff56627408e591fb8032c1296a4
claudiovallejo <hola@claudiovallejo.mx>
2020-03-02 07:19:15 -0500

    Creando template dinamico y proxies para proyectos

--- config.rb
+++ config.rb
@@ -20,17 +20,14 @@ page '/*.txt', layout: false
 page '/index.html', :layout => "principal"
 page '/contacto.html', :layout => "principal"
 page '/nosotros.html', :layout => "principal"
+page '/proyecto.html', :layout => "proyectos"
 
 # Proxy pages
 # https://middlemanapp.com/advanced/dynamic-pages/
 
-# proxy(
-#   '/this-page-has-no-template.html',
-#   '/template-file.html',
-#   locals: {
-#     which_fake_page: 'Rendering a fake page with a local variable'
-#   },
-# )
+data.sitio.proyecto.each do |id, proyecto|
+  proxy "/proyectos/#{id}/index.html", "/proyecto.html", :locals => { :proyecto => proyecto }, :ignore => true
+end
 
 # Helpers
 # Methods defined in the helpers block are available in templates
