9a03a68d1f552cdeb1275531bc2edd8cb413715d
claudiovallejo <hola@claudiovallejo.mx>
2020-03-06 15:14:20 -0500

    Borre archivos de static

--- static/javascript/contacto.js
+++
@@ -1,9 +0,0 @@
-function myMap() {
-    var myProp = {
-        center: new google.maps.LatLng(25.663801,-100.374796),
-        zoom:16,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMaps"),myProp);
-}
-
-myMap();
