db7ae52b6396c6eeaa1f7d66e4c46241d0c2b270
claudiovallejo <hola@claudiovallejo.mx>
2020-03-16 16:47:38 -0400

    Comprimiendo html, css, y js

--- gulpfile.js
+++ gulpfile.js
@@ -7,22 +7,40 @@ const gzip = require("gulp-gzip");
 function minifyHtml(cb) {
   gulp.src("build/**/*.html")
     .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("build/"));
+  cb();
+}
+
+function compressHtml(cb) {
+  gulp.src("build/**/*.html")
     .pipe(gzip())
     .pipe(gulp.dest("build/"));
   cb();
 }
-  
+
 function minifyCss(cb) {
   gulp.src("build/stylesheets/*.css")
     .pipe(cssmin())
+    .pipe(gulp.dest("build/stylesheets/"));
+  cb();
+}
+
+function compressCss(cb) {
+  gulp.src("build/stylesheets/*.css")
     .pipe(gzip())
     .pipe(gulp.dest("build/stylesheets/"));
   cb();
 }
-  
+
 function minifyJs(cb) {
   gulp.src("build/javascripts/*.js")
-    .pipe(jsmin())
+  .pipe(jsmin())
+  .pipe(gulp.dest("build/javascripts/"));
+  cb();
+}
+
+function compressJs(cb) {
+  gulp.src("build/javascripts/*.js")
     .pipe(gzip())
     .pipe(gulp.dest("build/javascripts/"));
   cb();
@@ -30,6 +48,9 @@ function minifyJs(cb) {
 
 exports.default = gulp.series(
   minifyHtml,
+  compressHtml,
   minifyCss,
+  compressCss,
   minifyJs,
+  compressJs,
 );
