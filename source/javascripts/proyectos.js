49edf985ddb925b109d8d5392692192f9ff3b821
bvallejop <bvallejop@gmail.com>
2020-04-19 13:58:12 -0500

    cambio imagenes

--- source/javascripts/proyectos.js
+++ source/javascripts/proyectos.js
@@ -1,68 +1,23 @@
 const imagenPrincipal = document.getElementById("imagen-principal");
-const imagen1 = document.getElementById("imagen-1");
-const imagen1Url = imagen1.getAttribute("src");
-const imagen2 = document.getElementById("imagen-2");
-const imagen2Url = imagen2.getAttribute("src");
-const imagen3 = document.getElementById("imagen-3");
-const imagen3Url = imagen3.getAttribute("src");
-const imagen4 = document.getElementById("imagen-4");
-const imagen4Url = imagen4.getAttribute("src");
-const imagen5 = document.getElementById("imagen-5");
-const imagen5Url = imagen5.getAttribute("src");
-const imagen6 = document.getElementById("imagen-6");
-const imagen6Url = imagen6.getAttribute("src");
-const imagen7 = document.getElementById("imagen-7");
-const imagen7Url = imagen7.getAttribute("src");
-const imagen8 = document.getElementById("imagen-8");
-const imagen8Url = imagen8.getAttribute("src");
+const imagenesGaleria = document.getElementsByClassName("js-imagen-galeria");
 
-imagen1.addEventListener("click", function() {
-  imagenPrincipal.setAttribute("src", imagen1Url);
-  seleccionarImagen(imagen1);
-});
+for (var i = 0; i < imagenesGaleria.length; i++) {
+  let imagenUrl = imagenesGaleria[i].getAttribute("src");
 
-imagen2.addEventListener("click", function() {
-  imagenPrincipal.setAttribute("src", imagen2Url);
-  seleccionarImagen(imagen2);
-});
-
-imagen3.addEventListener("click", function() {
-  imagenPrincipal.setAttribute("src", imagen3Url);
-  seleccionarImagen(imagen3);
-});
-
-imagen4.addEventListener("click", function() {
-  imagenPrincipal.setAttribute("src", imagen4Url);
-  seleccionarImagen(imagen4);
-});
-
-imagen5.addEventListener("click", function() {
-  imagenPrincipal.setAttribute("src", imagen5Url);
-  seleccionarImagen(imagen5);
-});
-
-imagen6.addEventListener("click", function() {
-  imagenPrincipal.setAttribute("src", imagen6Url);
-  seleccionarImagen(imagen6);
-});
-
-imagen7.addEventListener("click", function() {
-  imagenPrincipal.setAttribute("src", imagen7Url);
-  seleccionarImagen(imagen7);
-});
-
-imagen8.addEventListener("click", function() {
-  imagenPrincipal.setAttribute("src", imagen8Url);
-  seleccionarImagen(imagen8);
-});
+  imagenesGaleria[i].addEventListener("click", () => {
+    imagenPrincipal.setAttribute("src", imagenUrl);
+    seleccionarImagen(imagenesGaleria[i]);
+  });
+}
 
 const seleccionarImagen = (imagen) => {
   const imagenSeleccionada = document.getElementsByClassName("o-100pt-l")[0];
+  
   imagenSeleccionada.classList.remove("o-100pt-l");
   imagenSeleccionada.classList.add("o-35pt-l");
 
-  imagen.classList.remove("o-35pt-l");
-  imagen.classList.add("o-100pt-l");
+  // imagen.classList.remove("o-35pt-l");
+  // imagen.classList.add("o-100pt-l");
 }
 
 const detalles = document.querySelectorAll("[data-detalle]");
