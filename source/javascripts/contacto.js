a156cecc68f5eb058bdbc509cac5e557b15fc40d
bvallejop <bvallejop@gmail.com>
2020-07-02 13:21:51 -0500

    se cambio ubicacion en google maps y se cambio direccion en contacot

--- source/javascripts/contacto.js
+++ source/javascripts/contacto.js
@@ -1,6 +1,6 @@
 function myMap() {
   var myProp = {
-      center: new google.maps.LatLng(25.663801,-100.374796),
+      center: new google.maps.LatLng(25.657563,-100.365558),
       zoom: 16,
   };
   var map = new google.maps.Map(document.getElementById("googleMaps"), myProp);
